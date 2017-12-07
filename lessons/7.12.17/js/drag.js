d87e89ab31fac177d66b54a948ca432d06f12195
Darlane <darlanedelin@gmail.com>
2017-12-07 19:58:00 +0300

    cross game

--- lessons/7.12.17/js/drag.js
+++ lessons/7.12.17/js/drag.js
@@ -7,7 +7,8 @@
         pointTwoPos = pointTwo.getBoundingClientRect(),
         maxLeft = field.offsetWidth - point.offsetWidth * 1.5,
         maxTop = field.offsetHeight - point.offsetHeight * 1.5;
-
+    // pointTwo.style.top = Math.floor(Math.random()*380 + 1) + 'px';
+    // pointTwo.style.left = Math.floor(Math.random()*380 + 1) + 'px';
     document.onkeydown = function (event) {
         if (event.keyCode === 39 && pointOnePos.left < maxLeft) {
             point.style.left = (pointOnePos.left - 1 + step) + 'px';
@@ -15,10 +16,10 @@
         if (event.keyCode === 40 && pointOnePos.top < maxTop) {
             point.style.top = (pointOnePos.top - 1 + step) + 'px';
         }
-        if (event.keyCode === 37 && pointOnePos.left > 0) {
+        if (event.keyCode === 37 && pointOnePos.left > step) {
             point.style.left = (pointOnePos.left - 1 - step) + 'px';
         }
-        if (event.keyCode === 38 && pointOnePos.top > 0) {
+        if (event.keyCode === 38 && pointOnePos.top > step) {
             point.style.top = (pointOnePos.top - 1 - step) + 'px';
         }
         pointOnePos = point.getBoundingClientRect();
